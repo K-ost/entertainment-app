5e06f6c5cba24e4900ccb2b130d01dd90ca003de
K-ost <golubkovru@gmail.com>
2024-08-29 11:26:06 +0300

    adding test for debounce

--- src/hooks/useDebounce.test.tsx
+++ src/hooks/useDebounce.test.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { describe, expect, it, vi } from "vitest";
 import { render, renderHook, screen } from "@testing-library/react";
 import { userEvent } from "@testing-library/user-event";
@@ -9,5 +10,37 @@ describe("useDebounce", () => {
   it("Should return a function", async () => {
     const { result } = renderHook(() => useDebounce(mockedFn, 500));
     expect(typeof result.current).toBe("function");
+  });
+
+  it("Debounce with delay", async () => {
+    const setState = vi.fn();
+    const useStateSpy = vi.spyOn(React, "useState");
+    useStateSpy.mockImplementation(() => ["", setState]);
+
+    const { result } = renderHook(() => useDebounce(() => setState(), 100));
+
+    render(
+      <input type="text" onChange={() => result.current()} placeholder="type" />
+    );
+    const textField = screen.getByPlaceholderText("type");
+    await userEvent.type(textField, "test", { delay: 98 });
+
+    expect(setState).toBeCalledTimes(1);
+  });
+
+  it("Debounce without delay", async () => {
+    const setState = vi.fn();
+    const useStateSpy = vi.spyOn(React, "useState");
+    useStateSpy.mockImplementation(() => ["", setState]);
+
+    const { result } = renderHook(() => useDebounce(() => setState(), 10));
+
+    render(
+      <input type="text" onChange={() => result.current()} placeholder="type" />
+    );
+    const textField = screen.getByPlaceholderText("type");
+    await userEvent.type(textField, "test", { delay: 20 });
+
+    expect(setState).toBeCalledTimes(4);
   });
 });
