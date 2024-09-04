d90f23a75d6c4e926bb3aa94c0a7b856b1b4ecda
K-ost <golubkovru@gmail.com>
2024-09-04 18:53:07 +0300

    playbutton

--- src/ui/Play.tsx
+++ src/ui/Play.tsx
@@ -2,15 +2,16 @@ import { BoxProps, Button, styled } from "@mui/material";
 import play from "../assets/play.svg";
 
 // Styled
-export const PlayButton = styled(Button)<BoxProps>(() => ({
+export const PlayButton = styled(Button)<BoxProps>(({ theme }) => ({
   backgroundColor: "rgba(255, 255, 255, 0.25)",
   backgroundImage: `url(${play})`,
   backgroundPosition: "9px center",
   backgroundRepeat: "no-repeat",
   borderRadius: "48px",
   content: '"Play"',
+  color: theme.palette.common.white,
   display: "none",
-  fontSize: "var(--XS)",
+  fontSize: theme.typography.fontSize,
   fontWeight: 300,
   lineHeight: "20px",
   padding: "14px 24px 14px 58px",
@@ -20,6 +21,9 @@ export const PlayButton = styled(Button)<BoxProps>(() => ({
   top: "50%",
   transform: "translate(-50%,-50%)",
   zIndex: 5,
+  "&:hover": {
+    backgroundColor: "rgba(255, 255, 255, 0.15)",
+  },
 }));
 
 const Play = (): JSX.Element => {
