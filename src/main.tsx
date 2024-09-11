52d383467cca13c5fab482c630f2a44bbc96db5b
K-ost <golubkovru@gmail.com>
2024-09-11 19:07:59 +0300

    updata package json 3

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "@mui/material";
 import { customTheme } from "./theme.ts";
@@ -12,13 +12,13 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <QueryClientProvider client={queryClient}>
         <ThemeProvider theme={customTheme}>
           <MainStyles />
           <App />
         </ThemeProvider>
       </QueryClientProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
