3606965039e46c19bf27a96a0e55cceec39f4679
K-ost <golubkovru@gmail.com>
2024-09-13 20:16:23 +0300

    removing json server and concurrently

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,1 @@
-export const API_URL = "http://localhost:8000";
-// export const API_URL =
-//   "https://my-json-server.typicode.com/K-ost/entertainment-app";
+export const API_URL = "https://entertainment-server-r6f4.onrender.com/api";
